07679d4398d60408632a75555b554dedf6083762
rodopb <pbalamrodolfo@gmail.com>
2017-01-30 18:06:16 -0600

    Add files via upload

---
+++ practica9/app.js
@@ -0,0 +1,16 @@
+'use strict';
+angular.module('myApp', [
+'myApp.controllers',
+'ngRoute'
+]);
+angular.module('myApp').config(function($routeProvider,
+$locationProvider){
+$routeProvider.when('/view1',{
+controller:'Controller1',
+templateUrl:'/partials/view1.html'
+}).when('/view2/:firstname/:lastname',{
+controller: 'Controller2',
+templateUrl: '/partials/view2.html'
+}).otherwise({redirectTo:'/view1'});
+$locationProvider.html5Mode(true);
+});
